b82e4eca5980333fef5d2bf093869d695c32bf9f
Albert Ho <albertcyi.ho@gmail.com>
2022-02-12 11:26:41 -0800

    Add seed queries

---
+++ db/seeds/02_resources.sql
@@ -0,0 +1,26 @@
+-- Widgets table seeds here (Example)
+INSERT INTO resources (owner_id, title, description, type, topic, url)
+VALUES (
+    1,
+    'Goodbye Ring',
+    'My proudest moment!',
+    'Video',
+    'Nature',
+    'https://www.youtube.com/watch?v=c24-0Amwyik&ab_channel=EgalmothOfGondolin01'
+  ),
+  (
+    2,
+    'The Ring',
+    'My preciousssssssss!',
+    'Blog',
+    'Jewelry',
+    'https://lotr.fandom.com/wiki/One_Ring'
+  ),
+  (
+    3,
+    'Me IRL',
+    'Read about the real Gandalf',
+    'Blog',
+    'Writing',
+    'https://mckellen.com/blog/index.htm'
+  );
