57135743eb7927cf543adf5876560d1c1396eeb0
Lee Pavelich <leepavelich@gmail.com>
2022-02-12 18:27:47 +0000

    Create users and resources schemas

---
+++ db/schema/02_resources.sql
@@ -0,0 +1,16 @@
+-- Drop and recreate Resources table
+
+DROP TABLE IF EXISTS resources CASCADE;
+
+
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id),
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  type VARCHAR(255) NOT NULL,
+  topic VARCHAR(255),
+  url VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  is_deleted BOOLEAN DEFAULT false
+);
