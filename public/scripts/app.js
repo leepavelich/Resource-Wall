97bcf9c60467e6f68c181f9f4e3ca5d4ecc1c129
Lee Pavelich <leepavelich@gmail.com>
2022-02-13 22:51:59 +0000

    Port over Tweet base code and successfully jQuery in api/resource

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,76 @@
-// Client facing scripts here
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ */
+
+$(() => {
+  loadResources(); // initial page load
+  // newTweetIconClick();  // top-right compose button
+  // resourceSubmission();  // compose tweet box
+  scrollToTopButton();  // bottom-right scroll-to-top button
+});
+
+const createResourceElement = (resource) => {
+  const timeAgo = timeago.format(resource.created_at);
+
+  const $resource = `
+  <article class="resource">
+    <header>
+      <image class="avatar" src=avatar></image>
+      <div class="name"></div>
+      <div class="handle"></div>
+    </header>
+    <div class="resource-content">
+      <p class="resource-content-text"></p>
+    </div>
+    <footer>
+      <div class="days-ago">${timeAgo}</div>
+      <div class="icons">
+        <span><i class="fas fa-flag" id="flag"></i></span>
+        <span><i class="fas fa-retweet" id="retweet"></i></span>
+        <span><i class="fas fa-heart" id="like"></i></span>
+      </div>
+    </footer>
+  </article>
+  `;
+  return $resource;
+};
+
+// escape XSS
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+// load resources
+
+const loadResources = () => {
+  $.get('/api/resources', renderResources);
+};
+
+const renderResources = (resourcesObj) => {
+  const $resourcesContainer = $('.resource-container');
+  $resourcesContainer.empty();
+
+  resourcesObj.resources.forEach(resource => {
+    const $resource = createResourceElement(resource);
+    $resourcesContainer.prepend($resource);
+  });
+};
+
+const scrollToTopButton = () => {
+  const $scrollToTop = $('.scroll-to-top');
+
+  $(window).scroll(function() {
+    $(this).scrollTop() > 200
+      ? $scrollToTop.fadeIn().css('display', 'flex')
+      : $scrollToTop.fadeOut();
+  });
+
+  $scrollToTop.click(() => {
+    const $new = $('#new-tweet-form');
+    $new.slideDown('fast');
+    $('#tweet-text').focus();
+  });
+};
