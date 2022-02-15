4b54ac99c8520c29ae5ea561a6cb2aefa3de4bff
Lee Pavelich <leepavelich@gmail.com>
2022-02-15 17:51:15 +0000

    Replaced hardcoded 'gollum' with user_id from cookie

--- public/scripts/helpers/likes.js
+++ public/scripts/helpers/likes.js
@@ -1,12 +1,15 @@
 
 const renderLikes = (data) => {
   const resourceLikes = data.resources;
+  const currentUserId = Number(document.cookie.split("=")[1]);
+
+  console.log(currentUserId);
 
   for (const item of resourceLikes) {
     const resourceId = item.id;
     $.get(`/api/resources/${resourceId}/likes`)
       .then((response) => {
-        if (response.resources.some(e => e.username === 'gollum')) {  // TODO: pass in user_id
+        if (response.resources.some(e => e.user_id === currentUserId)) {
           likesToggle(resourceId);
         }
       });
