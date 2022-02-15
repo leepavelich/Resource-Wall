4bd1eb6a401c1783ddaebb5c589d55c16b9aa8f2
Albert Ho <albertcyi.ho@gmail.com>
2022-02-14 20:32:15 -0800

    Refactor dynamic styling logic, add hover effects

--- public/scripts/helpers/ratings.js
+++ public/scripts/helpers/ratings.js
@@ -28,94 +28,47 @@ const addRating = (id) => {
 
 const renderStars = (id) => {
   $.get(`/api/resources/${id}/rating`).then((data) => {
-    let avgRating = "0";
-    let numRatings = "0";
+    let avgRating = 0;
+    let numRatings = 0;
+
     if (data.resources) {
-      avgRating = data.resources.average_rating;
-      numRatings = data.resources.num_ratings;
+      avgRating = Number(data.resources.average_rating);
+      numRatings = Number(data.resources.num_ratings);
     }
-    const $ratingsText = numRatings === "1" ? "rating" : "ratings";
+
+    const $ratingsText = numRatings === 1 ? "rating" : "ratings";
     $(`#rate-${id} > p`).text(`${numRatings} ${$ratingsText}`);
 
-    switch (avgRating) {
-      case "0":
-        $(`#rate-${id} > .fa-regular.star-1`).show();
-        $(`#rate-${id} > .fa-regular.star-2`).show();
-        $(`#rate-${id} > .fa-regular.star-3`).show();
-        $(`#rate-${id} > .fa-regular.star-4`).show();
-        $(`#rate-${id} > .fa-regular.star-5`).show();
+    for (let i = 1; i <= 5; i++) {
+      $(`#rate-${id} .star-${i}`).mouseover(() => animateHover(id, i));
+      $(`#rate-${id}`).mouseout(() => removeColor(id, i));
 
-        $(`#rate-${id} > .fa-solid.star-1`).hide();
-        $(`#rate-${id} > .fa-solid.star-2`).hide();
-        $(`#rate-${id} > .fa-solid.star-3`).hide();
-        $(`#rate-${id} > .fa-solid.star-4`).hide();
-        $(`#rate-${id} > .fa-solid.star-5`).hide();
-        break;
-      case "1":
-        $(`#rate-${id} > .fa-solid.star-1`).show();
-        $(`#rate-${id} > .fa-regular.star-2`).show();
-        $(`#rate-${id} > .fa-regular.star-3`).show();
-        $(`#rate-${id} > .fa-regular.star-4`).show();
-        $(`#rate-${id} > .fa-regular.star-5`).show();
+      if (i <= avgRating) {
+        $(`#rate-${id} .fa-regular.star-${i}`).hide();
+        $(`#rate-${id} .fa-solid.star-${i}`).show();
+      } else {
+        $(`#rate-${id} .fa-regular.star-${i}`).show();
 
-        $(`#rate-${id} > .fa-regular.star-1`).hide();
-        $(`#rate-${id} > .fa-solid.star-2`).hide();
-        $(`#rate-${id} > .fa-solid.star-3`).hide();
-        $(`#rate-${id} > .fa-solid.star-4`).hide();
-        $(`#rate-${id} > .fa-solid.star-5`).hide();
-        break;
-      case "2":
-        $(`#rate-${id} > .fa-solid.star-1`).show();
-        $(`#rate-${id} > .fa-solid.star-2`).show();
-        $(`#rate-${id} > .fa-regular.star-3`).show();
-        $(`#rate-${id} > .fa-regular.star-4`).show();
-        $(`#rate-${id} > .fa-regular.star-5`).show();
-
-        $(`#rate-${id} > .fa-regular.star-1`).hide();
-        $(`#rate-${id} > .fa-regular.star-2`).hide();
-        $(`#rate-${id} > .fa-solid.star-3`).hide();
-        $(`#rate-${id} > .fa-solid.star-4`).hide();
-        $(`#rate-${id} > .fa-solid.star-5`).hide();
-        break;
-      case "3":
-        $(`#rate-${id} > .fa-solid.star-1`).show();
-        $(`#rate-${id} > .fa-solid.star-2`).show();
-        $(`#rate-${id} > .fa-solid.star-3`).show();
-        $(`#rate-${id} > .fa-regular.star-4`).show();
-        $(`#rate-${id} > .fa-regular.star-5`).show();
-
-        $(`#rate-${id} > .fa-regular.star-1`).hide();
-        $(`#rate-${id} > .fa-regular.star-2`).hide();
-        $(`#rate-${id} > .fa-regular.star-3`).hide();
-        $(`#rate-${id} > .fa-solid.star-4`).hide();
-        $(`#rate-${id} > .fa-solid.star-5`).hide();
-        break;
-      case "4":
-        $(`#rate-${id} > .fa-solid.star-1`).show();
-        $(`#rate-${id} > .fa-solid.star-2`).show();
-        $(`#rate-${id} > .fa-solid.star-3`).show();
-        $(`#rate-${id} > .fa-solid.star-4`).show();
-        $(`#rate-${id} > .fa-regular.star-5`).show();
-
-        $(`#rate-${id} > .fa-regular.star-1`).hide();
-        $(`#rate-${id} > .fa-regular.star-2`).hide();
-        $(`#rate-${id} > .fa-regular.star-3`).hide();
-        $(`#rate-${id} > .fa-regular.star-4`).hide();
-        $(`#rate-${id} > .fa-solid.star-5`).hide();
-        break;
-      case "5":
-        $(`#rate-${id} > .fa-solid.star-1`).show();
-        $(`#rate-${id} > .fa-solid.star-2`).show();
-        $(`#rate-${id} > .fa-solid.star-3`).show();
-        $(`#rate-${id} > .fa-solid.star-4`).show();
-        $(`#rate-${id} > .fa-solid.star-5`).show();
-
-        $(`#rate-${id} > .fa-regular.star-1`).hide();
-        $(`#rate-${id} > .fa-regular.star-2`).hide();
-        $(`#rate-${id} > .fa-regular.star-3`).hide();
-        $(`#rate-${id} > .fa-regular.star-4`).hide();
-        $(`#rate-${id} > .fa-regular.star-5`).hide();
-        break;
+        $(`#rate-${id} .fa-solid.star-${i}`).hide();
+      }
     }
   });
+};
+
+const animateHover = (resourceId, starIndex) => {
+  for (let i = 1; i <= 5; i++) {
+    if (i <= starIndex) {
+      $(`#rate-${resourceId} .fa-solid.star-${i}`).css("color", "gold");
+      $(`#rate-${resourceId} .fa-regular.star-${i}`).css("color", "gold");
+    }
+  }
+};
+
+const removeColor = (resourceId, starIndex) => {
+  for (let i = 1; i <= 5; i++) {
+    if (i <= starIndex) {
+      $(`#rate-${resourceId} .fa-solid.star-${i}`).css("color", "goldenrod");
+      $(`#rate-${resourceId} .fa-regular.star-${i}`).css("color", "goldenrod");
+    }
+  }
 };
