8ad4dba79685514f715bdc3ee2fa36ba48644656
Albert Ho <albertcyi.ho@gmail.com>
2022-02-15 20:07:26 -0800

    Fix - remove username on logout

--- public/scripts/helpers/logout.js
+++ public/scripts/helpers/logout.js
@@ -2,6 +2,7 @@ const logout = () => {
   $("#auth-btn").on("click", function () {
     $.get("/auth/logout").then(() => {
       $(this).html("Login");
+      $("#current-user").html("");
       $(".show-if-auth").hide();
       loadResources();
     });
