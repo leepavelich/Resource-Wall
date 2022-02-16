d1ad0e71eada580834ce524697f999f26914eb7b
Albert Ho <albertcyi.ho@gmail.com>
2022-02-16 15:04:51 -0800

    Refactor - add helper variables for code readability

--- public/scripts/helpers/delete.js
+++ public/scripts/helpers/delete.js
@@ -1,7 +1,9 @@
 const addDeleteListener = (resource) => {
   $(`#delete-resource-${resource.id}`).on("click", function () {
-    const currentUserId = document.cookie.split("=")[1];
-    if (Number(resource.user_id) === Number(currentUserId)) {
+    const currentUserId = Number(document.cookie.split("=")[1]);
+    const resourceOwnerId = Number(resource.user_id);
+
+    if (currentUserId === resourceOwnerId) {
       const { id } = resource;
       $.post("/api/resources/remove", { id }).then(() => {
         $(this).closest("article").hide();
@@ -11,9 +13,11 @@ const addDeleteListener = (resource) => {
 };
 
 const addDeleteCommentListener = (comment) => {
-  const currentUserId = document.cookie.split("=")[1];
   $(`#delete-comment-${comment.comment_id}`).on("click", function () {
-    if (Number(comment.user_id) === Number(currentUserId)) {
+    const currentUserId = Number(document.cookie.split("=")[1]);
+    const resourceOwnerId = Number(resource.user_id);
+
+    if (currentUserId === resourceOwnerId) {
       const { comment_id } = comment;
       $.post("/api/resources/comments/remove", { comment_id }).then(() => {
         $(this).closest(".comment-container").hide();
