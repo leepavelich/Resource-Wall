9a3a001f7a4ecb12d15c599b87313a9537d8ae44
Albert Ho <albertcyi.ho@gmail.com>
2022-02-17 14:13:30 -0800

    Refactor - update db

--- db/schema/03_likes.sql
+++ db/schema/03_likes.sql
@@ -1,7 +1,5 @@
 -- Drop and recreate Likes table
-
 DROP TABLE IF EXISTS likes CASCADE;
-
 CREATE TABLE likes (
   user_id INTEGER REFERENCES users(id),
   resource_id INTEGER REFERENCES resources(id)
--- db/schema/05_comments.sql
+++ db/schema/05_comments.sql
@@ -1,7 +1,5 @@
 -- Drop and recreate Comments table
-
 DROP TABLE IF EXISTS comments CASCADE;
-
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
